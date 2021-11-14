24e0bac0e278242ebc3992c944479ba4e4efd12b
MattiasY <mattiasx19@gmail.com>
2021-11-14 15:01:30 -0800

    step3

--- project.py
+++ project.py
@@ -5,8 +5,14 @@ import random
 import nltk
 import numpy as np
 from nltk.stem import WordNetLemmatizer
-import tensorflow as tf
+""" import tensorflow as tf
 from tf.keras import Sequential
-from tf.keras.layers import Dense, Dropout
+from tf.keras.layers import Dense, Dropout """
 nltk.download("punkt")
 nltk.download("wordnet")
+
+data_file = open("/Users/brutt/Hackathon/Project/intents.json",)
+data = json.load(data_file)
+
+print(data)
+print("test")
