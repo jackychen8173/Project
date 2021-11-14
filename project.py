670e20d1f996c691b3e7313a29ca40f870507af3
MattiasY <mattiasx19@gmail.com>
2021-11-14 14:39:06 -0800

    libraries (working)

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ import nltk
 import numpy as np
 from nltk.stem import WordNetLemmatizer
 import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+from tf.keras import Sequential
+from tf.keras.layers import Dense, Dropout
 nltk.download("punkt")
 nltk.download("wordnet")
